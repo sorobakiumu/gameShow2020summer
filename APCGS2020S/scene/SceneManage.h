856d1efd74629b211f7c22b3ddeda65dfc047617
sorobakiumu <bakatesuto_soro@ezweb.ne.jp>
2020-05-13 16:06:38 +0900

    描画

--- APCGS2020S/scene/SceneManage.h
+++ APCGS2020S/scene/SceneManage.h
@@ -1,6 +1,12 @@
 #pragma once
 #include <Windows.h>
+#include<vector>
+#include <tuple>
 #include "../Common/Vector2.h"
+#include "BaseScene.h"
+
+
+using DRAW_QUE = std::tuple<Vec2double,double,double,int>;		//座標、サイズ、角度、ハンドル
 
 class SceneManage
 {
@@ -31,9 +37,17 @@ public:
 	const Vec2Int ScreenSize;
 	Vec2Int ScreenCenter;
 
+	//描画したいものをキューに追加する
+	void addDrawQue(DRAW_QUE dque);
+
 	void Run();
 private:
 	static SceneManage* sInstance;
+
+	unique_Base _activeScene;		//<BaseScene>を、使っているのがいなくなったら消える
+
+	std::vector<DRAW_QUE> _drawList;	//描画したいリスト
+	void Draw();
 
 	SceneManage();
 	~SceneManage();
