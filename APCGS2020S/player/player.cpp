13f2f0d24e5f85e2c57bcd765ba19c4c2be61eb7
Izumi5468 <65134792+Izumi5468@users.noreply.github.com>
2020-05-15 11:24:36 +0900

    player試作改二
    player移動関連でち

--- APCGS2020S/player/player.cpp
+++ APCGS2020S/player/player.cpp
@@ -23,54 +23,94 @@ player::player(Vec2double _pos, Vec2Int _size)
 	Init();
 }
 
+// playerのﾃﾞｽﾄﾗｸﾀ
 player::~player()
 {
 
 }
 
-
-void player::Update(void)
+// ﾌﾟﾚｲﾔｰ情報の更新
+void player::UpDate()
 {
-
+	// 操作方法によって変えます
 	switch (ptn)
 	{
+	// ｷｰﾎﾞｰﾄﾞ使用時
 	case KeyBoard:
 
+		// 十字ｷｰ(左)入力時、左移動
 		if (CheckHitKey(KEY_INPUT_LEFT) == 1)
 		{
 			Pos.x = Pos.x - 5.0;
 		}
+		// 十字ｷｰ(右)入力時、右移動
 		else if (CheckHitKey(KEY_INPUT_RIGHT) == 1)
 		{
 			Pos.x = Pos.x + 5.0;
 		}
-				 
+
+		// 十字ｷｰ(上)入力時、跳躍
 		if (CheckHitKey(KEY_INPUT_UP) == 1 && jmpFlg == 0)
 		{
 			jpCtlFlag = 1;
 		}
 		break;
 
+	// ｹﾞｰﾑﾊﾟｯﾄﾞ?使用時
 	case Pad:
+
+		// ﾊﾟｯﾄﾞ(左)入力時、左移動
+		if (GetJoypadInputState(PAD_INPUT_LEFT) == 1)
+		{
+			Pos.x = Pos.x - 5.0;
+		}
+		// ﾊﾟｯﾄﾞ(右)入力時、右移動
+		else if (GetJoypadInputState(PAD_INPUT_RIGHT) == 1)
+
+		{
+			Pos.x = Pos.x + 5.0;
+		}
+
+		// ﾊﾟｯﾄﾞ(上)入力時、跳躍
+		if (GetJoypadInputState(PAD_INPUT_UP) == 1)
+		{
+			jpCtlFlag = 1;
+		}
 
 		break;
 
+	// 何らかのバグ
 	default:
 
 		break;
 
 	}
 
-
+	// 跳躍ﾌﾗｸﾞが立っているとき
 	if (jpCtlFlag == 1)
 	{
-		Pos.y = pow((sec - 180), 2) * (60/180) + lpSceneMng.ScreenSize.y - Size.y;
+		// 跳躍山なり移動用2次関数
+		Pos.y = pow((sec - jmpCnt), 2) * (64 / jmpCnt) + lpSceneMng.ScreenSize.y - Size.y;
 
+		// 経過時間計測
 		sec++;
+
+		// もし、着地したら
+		if (Pos.y >= lpSceneMng.ScreenSize.y )
+		{
+			// めり込まないように再調整
+			Pos.y = lpSceneMng.ScreenSize.y;
+			
+			// 跳躍ﾌﾗｸﾞ戻し
+			jpCtlFlag = 0;
+
+			// 経過時間観測用変数初期化
+			sec = 0;
+		}
 	}
 
-
 }
+
 
 void player::Init(void)
 {
--- APCGS2020S/player/player.h
+++ APCGS2020S/player/player.h
@@ -2,7 +2,9 @@
 #include "..\Common\Vector2.h"
 #include "../Obj/Obj.h"
 #include "../scene/SceneManage.h"
+#include "../scene/GameScene.h"
 
+// 操作識別構造体
 enum Ctl
 {
 	KeyBoard,
@@ -15,21 +17,23 @@ class player :
 	public Obj
 {
 public:
-	player();									// ｺﾝｽﾄﾗｸﾀ
-	player(Vec2double pos, Vec2Int size);		// 指定付きｺﾝｽﾄﾗｸﾀ
-	~player();									// playerのﾃﾞｽﾄﾗｸﾀ
+	player();								// ｺﾝｽﾄﾗｸﾀ
+	player(Vec2double pos, Vec2Int size);	// 指定付きｺﾝｽﾄﾗｸﾀ
+	~player();								// playerのﾃﾞｽﾄﾗｸﾀ
 
-	int jpCtlFlag;
+	void UpDate() override;;				// player更新用関数
+	int jpCtlFlag;							// 跳躍制御ﾌﾗｸﾞ変数
+
 
 private:
 
-	void Update(void);
+	
+	
+	void Init(void);						// 初期化関数
 
-	void Init(void);							// 初期化関数
-
-	int jmpFlg;
-	int jmpCnt;
-	int sec;
+	int jmpFlg;								// 跳躍開始変数
+	int jmpCnt;								// 跳躍制御ﾌﾗｸﾞ変数
+	int sec;								// 跳躍制御ﾌﾗｸﾞ変数
 
 	Ctl ptn;
 };
