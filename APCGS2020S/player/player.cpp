3ec23fc01f52f38440a17a681b49c2c10e145121
Izumi5468 <65134792+Izumi5468@users.noreply.github.com>
2020-05-15 10:01:06 +0900

    player移動プロト
    playerの動作

--- APCGS2020S/player/player.cpp
+++ APCGS2020S/player/player.cpp
@@ -1,13 +1,10 @@
 #include "player.h"
-
-
+#include "DxLib.h"
 
 // playerのコンストラクタ
 player::player()
 {
-	// 初期化
 	Init();
-
 }
 
 // playerの初期座標とsizeを設定
@@ -28,10 +25,57 @@ player::player(Vec2double _pos, Vec2Int _size)
 
 player::~player()
 {
+
+}
+
+
+void player::Update(void)
+{
+
+	switch (ptn)
+	{
+	case KeyBoard:
+
+		if (CheckHitKey(KEY_INPUT_LEFT) == 1)
+		{
+			Pos.x = Pos.x - 5.0;
+		}
+		else if (CheckHitKey(KEY_INPUT_RIGHT) == 1)
+		{
+			Pos.x = Pos.x + 5.0;
+		}
+				 
+		if (CheckHitKey(KEY_INPUT_UP) == 1 && jmpFlg == 0)
+		{
+			jpCtlFlag = 1;
+		}
+		break;
+
+	case Pad:
+
+		break;
+
+	default:
+
+		break;
+
+	}
+
+
+	if (jpCtlFlag == 1)
+	{
+		Pos.y = pow((sec - 180), 2) * (60/180) + lpSceneMng.ScreenSize.y - Size.y;
+
+		sec++;
+	}
+
+
 }
 
 void player::Init(void)
 {
-
-
+	sec = 0;
+	jmpFlg = 0;
+	jmpCnt = 180;
+	jpCtlFlag = 0;
 }
--- APCGS2020S/player/player.h
+++ APCGS2020S/player/player.h
@@ -1,5 +1,15 @@
 #pragma once
+#include "..\Common\Vector2.h"
 #include "../Obj/Obj.h"
+#include "../scene/SceneManage.h"
+
+enum Ctl
+{
+	KeyBoard,
+	Pad,
+	max
+
+};
 
 class player :
 	public Obj
@@ -9,11 +19,18 @@ public:
 	player(Vec2double pos, Vec2Int size);		// w’è•t‚«ºİ½Ä×¸À
 	~player();									// player‚ÌÃŞ½Ä×¸À
 
+	int jpCtlFlag;
 
 private:
 
+	void Update(void);
+
 	void Init(void);							// ‰Šú‰»ŠÖ”
 
+	int jmpFlg;
+	int jmpCnt;
+	int sec;
 
+	Ctl ptn;
 };
 
