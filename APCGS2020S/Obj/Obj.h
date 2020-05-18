fe65518969cd524365144e92c2b1c6b2a61ae7eb
Izumi5468 <65134792+Izumi5468@users.noreply.github.com>
2020-05-18 15:49:51 +0900

    virtualDraw追加しました

--- APCGS2020S/Obj/Obj.h
+++ APCGS2020S/Obj/Obj.h
@@ -1,6 +1,8 @@
 #pragma once
 #include<memory>
 #include "../Common/Vector2.h"
+#include <vector>
+
 enum class OBJ_ID
 {
 	PLAYER,
@@ -10,9 +12,22 @@ enum class OBJ_ID
 	MAX,
 };
 
+
+// 状態管理enum class
+enum class STATE
+{
+	NORMAL,			// 通常状態
+	EX,				// 特殊状態
+	DEATH,			// 死亡状態
+	MAX
+};
+
 class Obj;
 
+// 複数個所でﾎﾟｲﾝﾀｰ参照するための代物
 using SharedObj = std::shared_ptr<Obj>;
+
+using AnimVector = std::vector<std::pair<int, unsigned int>>;
 
 class Obj
 {
@@ -20,12 +35,36 @@ public:
 	Obj();
 	virtual ~Obj();
 
+	virtual void Draw(void) = 0;
+
 	const OBJ_ID GetID()const { return ID; }
 
+	// 座標取得用関数
 	const Vec2double GetPos()const { return Pos; }
+
+	// ｻｲｽﾞ取得用関数
 	const Vec2Int GetSize()const { return Size; }
+
+	// 死亡確定用関数
 	const bool Getdead()const { return dead; }
+
 	void SetPos(Vec2double pos) { Pos = pos; }
+
+	void Draw(int id);
+
+	void Draw(void);
+
+	bool State(const STATE state);
+
+	const STATE animKey(void) const;
+
+	bool SetAnim(const STATE state, AnimVector& data);
+
+	bool isAnimEnd(void);
+
+	bool isAlive(void);
+
+
 
 	virtual void UpDate() = 0;
 protected:
@@ -34,5 +73,11 @@ protected:
 	Vec2Int Size;
 
 	bool dead;
+	bool alive;
+
+private:
+
+	//std::map<STATE , Anim
+
 };
 
