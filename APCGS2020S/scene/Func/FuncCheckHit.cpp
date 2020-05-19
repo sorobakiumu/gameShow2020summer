d013d724c94e7c7ee8c7c9d7943dfda92bae76a8
1205Yuki <tongkouyongqi442@gmail.com>
2020-05-19 12:27:41 +0900

    CheckHitは死んだ。

--- APCGS2020S/scene/Func/FuncCheckHit.cpp
+++ APCGS2020S/scene/Func/FuncCheckHit.cpp
@@ -10,67 +10,8 @@ void FuncCheckHit::operator()(int no, void* scene,Vec2double pos)
 		if (Cnt == no)
 		{
 			Size = obj->GetSize();
-			for (int i=0;i<4;i++)
-			{
- 				CheckHit[i] = true;
-			}
-			while (CheckHit[0]||CheckHit[1]||CheckHit[2]||CheckHit[3])
-			{
-				Pos = obj->GetPos();
-
-
-				Vec2Int mapSize = { ((GameScene*)scene)->MapSize.x / 32,((GameScene*)scene)->MapSize.y / 32 };
-
-				if (((GameScene*)scene)->Map[(((Pos.y - Size.y / 2) / 32) * mapSize.x + ((Pos.x - Size.x / 2) / 32))] == 0)
-				{
-					CheckHit[0] = false;
-				}
-				if (((GameScene*)scene)->Map[(((Pos.y - Size.y / 2) / 32) * mapSize.x + ((Pos.x + Size.x / 2) / 32))] == 0)
-				{
-					CheckHit[1] = false;
-				}
-				if (((GameScene*)scene)->Map[(((Pos.y + Size.y / 2) / 32) * mapSize.x + ((Pos.x - Size.x / 2) / 32))] == 0)
-				{
-					CheckHit[2] = false;
-				}
-				if (((GameScene*)scene)->Map[(((Pos.y + Size.y / 2) / 32) * mapSize.x + ((Pos.x + Size.x / 2) / 32))] == 0)
-				{
-					CheckHit[3] = false;
-				}
-
-				if (CheckHit[0] == true || CheckHit[2] == true)
-				{
-					obj->SetPos({ Pos.x + 1,Pos.y });
-					Pos = obj->GetPos();
-				}
-				if (CheckHit[1] == true || CheckHit[3] == true)
-				{
-					obj->SetPos({ Pos.x - 1,Pos.y });
-					Pos = obj->GetPos();
-				}
-				if (CheckHit[0] == true || CheckHit[1] == true)
-				{
-					obj->SetPos({ Pos.x,Pos.y+1 });
-					Pos = obj->GetPos();
-				}
-				if (CheckHit[2] == true || CheckHit[3] == true)
-				{
-					obj->SetPos({ Pos.x,Pos.y - 1 });
-					Pos = obj->GetPos();
-				}
-
-				if (CheckHit[0] && CheckHit[1] && CheckHit[2] && CheckHit[3])
-				{
-					for (auto obj: ((GameScene*)scene)->ObjList)
-					{
-						if (obj->GetID()==OBJ_ID::PLAYER)
-						{
-							obj->SetPos(ScrCenter);
-						}
-					}
-				}
-			}
-
+			Pos = { (((GameScene*)scene)->MapSize.x / 2 - ((GameScene*)scene)->MapPos.x) + obj->GetPos().x,obj->GetPos().y };
+			// プレイヤーのサイズと座標(マップ座標)を取得する
 		}
 		Cnt++;
 	}
