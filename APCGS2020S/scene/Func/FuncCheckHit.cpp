530aca9fac436715d1ed6aeaf115f3d15942e5f1
1205Yuki <tongkouyongqi442@gmail.com>
2020-05-22 09:42:47 +0900

    Func

--- APCGS2020S/scene/Func/FuncCheckHit.cpp
+++ APCGS2020S/scene/Func/FuncCheckHit.cpp
@@ -8,6 +8,16 @@ bool FuncCheckHit::operator()(Vec2double pos,Vec2Int size)
 	{
 		return true;
 	}
-	;
-
+	if (GameScene::Map[(pos.y - size.y / 2) * MapSize.x / 32 + (pos.x + size.x / 2)] != 0)
+	{
+		return true;
+	}
+	if (GameScene::Map[(pos.y + size.y / 2) * MapSize.x / 32 + (pos.x - size.x / 2)] != 0)
+	{
+		return true;
+	}
+	if (GameScene::Map[(pos.y + size.y / 2) * MapSize.x / 32 + (pos.x + size.x / 2)] != 0)
+	{
+		return true;
+	}
 }
